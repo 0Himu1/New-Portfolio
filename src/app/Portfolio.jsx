e7964dbeab1019ee887b21092e3657ad52735bfd
0himu1 <0himu1@gmail.com>
2023-01-31 19:31:55 +0600

    Link added

--- src/app/Portfolio.jsx
+++ src/app/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PortfolioCard from './components/PortfolioCard';
+import data from '../pages/api/deta';
 
 export default function Portfolio() {
   return (
@@ -8,34 +9,18 @@ export default function Portfolio() {
         <h3 className="text-3xl py-1 dark:text-white font-bold">Portfolio</h3>
       </div>
       <div className="flex flex-col gap-10 py-10 md:flex-row md:flex-wrap">
-        <PortfolioCard
-          title="Tic-Tac-Toe"
-          text="simple tic tac toe game made by React js"
-          repo="#"
-          link="#"
-          tag={['Javascript', 'react']}
-        />
-        <PortfolioCard
-          title="Tic-Tac-Toe"
-          text="simple tic tac toe game made by React js"
-          repo="#"
-          link="#"
-          tag={['Javascript', 'react']}
-        />
-        <PortfolioCard
-          title="Tic-Tac-Toe"
-          text="simple tic tac toe game made by React js"
-          repo="#"
-          link="#"
-          tag={['Javascript', 'react']}
-        />
-        <PortfolioCard
-          title="Tic-Tac-Toe"
-          text="simple tic tac toe game made by React js"
-          repo="#"
-          link="#"
-          tag={['Javascript', 'react']}
-        />
+        {data.map(({
+          image, title, text, repo, link, tag,
+        }) => (
+          <PortfolioCard
+            image={image}
+            title={title}
+            text={text}
+            repo={repo}
+            link={link}
+            tag={tag}
+          />
+        ))}
       </div>
     </section>
   );
---
+++ src/pages/api/deta.js
@@ -0,0 +1,59 @@
+import ticTacToe from '../../../public/tic-tac-toe.jpg';
+import travel from '../../../public/travel-advisor.jpg';
+import advise from '../../../public/advise-generator.jpg';
+import tipCalc from '../../../public/tip-calculator.jpg';
+import nike from '../../../public/nike-promo.jpg';
+import todo from '../../../public/todo.jpg';
+
+const data = [
+  {
+    title: 'Tic Tac Toe',
+    text: 'A fun project using React class component',
+    repo: 'https://github.com/0Himu1/tic-tac-toe',
+    link: 'https://broken-tic-tac-toe.netlify.app/',
+    tag: ['JavaScript', 'React', 'CSS Grid'],
+    image: ticTacToe,
+  },
+  {
+    title: 'Travel Advisor',
+    text: 'Travel Advisor app using open-source API and Google Maps.',
+    repo: 'https://github.com/0Himu1/travel-advisor',
+    link: 'https://frabjous-halva-039204.netlify.app/',
+    tag: ['JavaScript', 'React', 'API', 'Ant Design'],
+    image: travel,
+  },
+  {
+    title: 'Advise Generator',
+    text: 'Generate random useful advice for fun.',
+    repo: 'https://github.com/0Himu1/advise-generator',
+    link: 'https://advisegenerator.netlify.app/',
+    tag: ['JavaScript', 'React', 'API'],
+    image: advise,
+  },
+  {
+    title: 'Tip Calculator',
+    text: 'Small tip calculator function using React Hooks',
+    repo: 'https://github.com/0Himu1/tip-calculator',
+    link: 'https://tip-calculaor.netlify.app/',
+    tag: ['JavaScript', 'React', 'React Hooks'],
+    image: tipCalc,
+  },
+  {
+    title: 'Nike Promo',
+    text: 'E-commerce page design using HTML & CSS',
+    repo: 'https://github.com/0Himu1/Nike-Promo-Page',
+    link: 'https://0himu1.github.io/Nike-Promo-Page/',
+    tag: ['HTML', 'CSS'],
+    image: nike,
+  },
+  {
+    title: 'ToDo App',
+    text: 'A simple to-do app using JavaScript',
+    repo: 'https://github.com/0Himu1/my-todo',
+    link: 'https://my-todo-nazmul.netlify.app/',
+    tag: ['JavaScript', 'HTML', 'CSS'],
+    image: todo,
+  },
+];
+
+export default data;
